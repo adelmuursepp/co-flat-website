8969b41be22045a7db2ca00bd376f461e48b5b4e
Adel Muursepp <adel.muursepp@gmail.com>
2021-04-02 19:49:55 +0300

    fb auth

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
     def avatar_url(user)
-    	if user.image
-    		"http://graph.facebook.com/#{user.uid}/picture?type=large"
-    	else
+    	# if user.image
+    	# 	"http://graph.facebook.com/#{user.uid}/picture?type=large"
+    	# else
 	       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-	       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
-	     end
+	    	return "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+	     #end
     end
 end
