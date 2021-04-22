0dd5132b0b9f509dfa78211b0e63331ff7ce19a1
Adel Muursepp <adel.muursepp@gmail.com>
2021-04-22 21:47:30 +0300

    added email notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://co-flat.com/" }
+  # config.action_mailer.default_url_options = { host: "https://co-flat.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://co-flat.com/' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['GMAIL'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
